e359fa592b42a2cde2dd46f2c9a6768597465222
ritikasource <ritika99.work@gmail.com>
2020-01-15 01:28:27 +0530

    Used jQuery 2.1.4 min version and added the functionality of creating new todos.

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,13 +1,25 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 
 });
 
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
 
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 
 event.stopPropogation();
+});
+
+$("input[type='text']").keypress(function(event){
+	
+	if(event.which === 13)
+	{
+		var todosText=$(this).val();
+	    $(this).val("");
+		$("ul").append("<li><span>X<span> " + todosText + "<li>");
+	 
+	}
+
 });
