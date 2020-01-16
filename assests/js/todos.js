981e7fc792bedfec6bc16b0e13b754c686221711
ritikasource <ritika99.work@gmail.com>
2020-01-17 00:23:40 +0530

    Added with further styling included icons and gradients.

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -3,14 +3,14 @@ $("ul").on("click", "li", function(){
 
 });
 
-$("ul").on("click","span",function(event){
 
+$("ul").on("click","span",function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
-
 event.stopPropagation();
 });
+
 
 $("input[type='text']").keypress(function(event){
 	
@@ -18,8 +18,12 @@ $("input[type='text']").keypress(function(event){
 	{
 		var todosText=$(this).val();
 	    $(this).val("");
-		$("ul").append("<li><span>X</span> " + todosText + "<li>");
+		$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todosText + "<li>")
 	 
 	}
 
+});
+
+$(".fa-pencil-square").click(function(){
+	$("input[type='text']").fadeToggle();
 });
